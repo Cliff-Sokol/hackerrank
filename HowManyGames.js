0a8932f17320dba73ec636db24e96234f3cbaad1
Cliff-Sokol <36664628+Cliff-Sokol@users.noreply.github.com>
2020-04-24 17:44:27 -0400

    Add files via upload

---
+++ HowManyGames.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the howManyGames function below.
+function howManyGames(p, d, m, s) {
+    // Return the number of games you can buy
+    var total = 0;
+    var numGames = 0;
+    var newPrice = p;
+    var discount = 0;
+
+    if (s < p) return numGames;
+
+    while (total <= s - m && total + newPrice <= s) {
+        numGames += 1;
+        total += newPrice;
+        newPrice -= d;
+        if (newPrice < m) {
+            newPrice = m;
+        }
+    }
+
+    return numGames;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const pdms = readLine().split(' ');
+
+    const p = parseInt(pdms[0], 10);
+
+    const d = parseInt(pdms[1], 10);
+
+    const m = parseInt(pdms[2], 10);
+
+    const s = parseInt(pdms[3], 10);
+
+    let answer = howManyGames(p, d, m, s);
+
+    ws.write(answer + "\n");
+
+    ws.end();
+}
